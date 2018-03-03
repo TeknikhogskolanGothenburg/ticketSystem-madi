9f21857defa76db2babce2eef55a499298ac96ba
madi77 <info@mdshop.se>
2018-03-03 09:15:52 +0100

    Projekt 3 två versioner
    Projekt3 HttpClient och RestSharp

---
+++ src/Projekt3/MVCWebCodeByMadi/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebCodeByMadi.Helper;
+using MVCWebCodeByMadi.Models;
+
+namespace MVCWebCodeByMadi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        VenuesRestClient venuesRestClient = new VenuesRestClient();
+
+        public IActionResult Venues()
+        {
+            List<Venues> venueList = new List<Venues>();
+            venueList = venuesRestClient.GetVenues();
+
+
+            return View(venueList);
+
+        }
+
+
+
+       
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+
+
+
+    }
+}
+
+       
+
+
+
+
+
+
+
+
+
+
+        
