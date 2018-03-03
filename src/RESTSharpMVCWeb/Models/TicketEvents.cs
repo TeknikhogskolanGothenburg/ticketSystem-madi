674bfbcf0eb817a90d6ebcdd699629ada7938029
madi77 <info@mdshop.se>
2018-03-03 12:52:36 +0100

    RESTSharp
    RESTSharp

---
+++ src/RESTSharpMVCWeb/Models/TicketEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCWebCodeByMadi.Models
+{
+    public class TicketEvents
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventHtmlDescription { get; set; }
+        public int TicketEventPrice { get; set; }
+
+    }
+}
